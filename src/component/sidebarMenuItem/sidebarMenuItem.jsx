ae5bde3e2bbf655d194fe67ff51c84d9406fce16
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-02-20 23:21:17 +0530

    animation add

--- src/component/sidebarMenuItem/sidebarMenuItem.jsx
+++ src/component/sidebarMenuItem/sidebarMenuItem.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import "./sidebarMenuItem.scss"
+import React from 'react';
+import './sidebarMenuItem.scss';
 import { NavLink } from 'react-router-dom';
 
 export const SidebarMenuItem = ({ route, icon, text, handleNav }) => {
     return (
         <div className="sidebar-menu-item" onClick={handleNav}>
-            <NavLink activeClassName="active" className='sidebar-menu-link'
-                to={`/${route}`}>
+            <NavLink className='sidebar-menu-link'
+                to={`/${route}`} activeclassname="active">
                 <div className='sidebar-menu-icon'>{icon}</div>
                 <span className='sidebar-menu-text'>{text}</span>
             </NavLink>
         </div>
-    )
-}
+    );
+};
