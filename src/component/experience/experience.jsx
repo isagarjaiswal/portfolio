9aa580fc1b19588334a57467b83a240f7ea5771b
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-02-22 11:46:51 +0530

    fix experinece bug

--- src/component/experience/experience.jsx
+++ src/component/experience/experience.jsx
@@ -1,10 +1,12 @@
+import React from 'react'
+import "./experience.scss"
 export const Experience = ({ data, head }) => {
     return (
         <div className="experience-section">
             <div className="experience-heading">{head}</div>
             <div className="experience-container">
-                {data.map((experience) => (
-                    <div key={experience.id}>
+                {data.map((experience, index) => (
+                    <div key={index}>
                         <div className="experience-item">
                             <div className="experience-date">{experience.date}</div>
                             <div className="experience-content">
@@ -15,9 +17,9 @@ export const Experience = ({ data, head }) => {
                                 {experience.description && <div className="experience-description">{experience.description}</div>}
                             </div>
                         </div>
+                        {data.length - 1 !== index && <div className="experience-row"></div>}
                     </div>
-                ))}
-            </div>
-        </div>
+                ))}</div>
+        </div >
     );
 };
