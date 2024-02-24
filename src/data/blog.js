653a901b43e821acc67c574477f4e631d7e0c5f7
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-02-24 22:18:24 +0530

    add link of scope or about

--- src/data/blog.js
+++ src/data/blog.js
@@ -2,12 +2,13 @@ export const blogsData = [{
     date: "Aug 2023",
     heading: "All About Scope",
     description: "In this article, we cover Scopes and their types, the Temporal Dead Zone, Lexical Scope, the Scope Chain, Shadowing in JavaScript, and the Global Object.",
-    mediumLink: "https://medium.com/@isagarjaiswal/all-about-scope-a9614a0d6361",
+    mediumLink: "https://medium.com/@isagarjaiswal/all-about-scope-3268fb1187f0",
     img: ""
 }, {
     date: "Sep 2023",
     heading: "All About Promise",
     description: "The Javascript code gets executed in two different ways “Synchronous” and “Asynchronous” are those exact two ways.",
-    mediumLink: "https://medium.com/@isagarjaiswal/all-about-promise-eaa89e4c4011",
+    mediumLink: "https://medium.com/@isagarjaiswal/all-about-6beb5141ebb0",
     img: ""
+    
 }]
