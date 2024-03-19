096dd47b8aa5823dce30597dfff88523d0e3c485
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-19 17:06:56 +0530

    fix some bug

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,13 +2,13 @@ export const experienceData = [
     {
         date: 'May 2023 – Present',
         role: 'SDE - 1',
-        company: 'HackerKernel',
+        company: 'Priyam Innovations Pvt. Ltd.',
         description: 'Responsible for creating REST APIs using Node.js with Express.js and MongoDB, implementing authentication flows using JWTs, salting and hashing with bcrypt, and developing protected routes in React and Node. Developed custom React hooks and components, wrote PRs on GitHub, and collaborated with cross-functional teams.'
     },
     {
         date: 'October 2022 – May 2023',
         role: 'Software Engineer-Intern',
-        company: 'HackerKernel',
+        company: 'Priyam Innovations Pvt. Ltd.',
         description: 'Contributed to writing React functional components using built-in hooks, created responsive designs using CSS Flexbox, and added native CSS animations to UI. Implemented features like modal, drawer, and tooltips using React Portal. Created PRs on GitHub, performed code reviews, and fixed deployment bugs.'
     }
 ];
