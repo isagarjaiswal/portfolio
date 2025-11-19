df5a58b24a670988c53c1f798432dd33511fcbac
Sagar Jaiswal <sagarjaiswal81555@gmail.com>
2025-11-20 01:07:58 +0530

    minor correct changes

--- src/component/index.js
+++ src/component/index.js
@@ -17,4 +17,4 @@ export { ProjectCard } from './projectCard/projectCard';
 export { SkillCard } from './skillCard/skillCard';
 export { NotFound } from './notFound/notFound';
 export { GAListener } from './GA/GAListener';
-export { loader } from './loader/loader';
+export { Loader } from './loader/loader';
